abae7c4d0e87e8720eea8ccdb27c561fd438cbe6
Nathalie <nathalie.kirch.nk@gmail.com>
2023-12-07 08:07:17 +0100

    intitialize basic git repo with open ai call

---
+++ court_judgements.py
@@ -0,0 +1,18 @@
+# Import api key from .enve file
+from dotenv import load_dotenv
+load_dotenv()
+import os
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+from openai import OpenAI
+client = OpenAI()
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
+
+print(completion.choices[0].message)
