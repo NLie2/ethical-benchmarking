0fd026bd1484b258592c188ee669484ba0497f49
Nathalie <nathalie.kirch.nk@gmail.com>
2024-04-02 10:43:26 +0200

    write experiment for context perturbations

--- triage_experiments/run_benchmark_with_contexts.py
+++ triage_experiments/run_benchmark_with_contexts.py
@@ -8,8 +8,9 @@ import argparse
 
 # Import key from .env file
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PRIVATE_ORG_KEY = os.getenv("PRIVATE_ORG_KEY")
 
-client = OpenAI(api_key=OPENAI_API_KEY)
+client = OpenAI(api_key=OPENAI_API_KEY, organization=PRIVATE_ORG_KEY)
 
 # Load files
 df = pd.read_csv("triage_experiments/datasets/triage_questions.csv")
@@ -19,25 +20,19 @@ triage_scenarios = json.load(open('triage_experiments/prompting/triage_scenarios
 ethics_prompts = json.load(open('prompting/ethics_prompts.json'))
 formatting_prompts = json.load(open('prompting/formatting_prompts.json'))
 
-contexts_changes = json.load(open('context_changes/prompt_for_nathalie.json'))
-contexts_changes = [contexts_changes["mad_scientist"], contexts_changes["healthcare_assistant"], contexts_changes["doctor_assitant"]]
+personas = json.load(open('context_changes/prompt_for_nathalie.json'))
+personas_names = ["healthcare_assistant", "mad_scientist", "doctor_assitant"]
 
 # Set default conditions
 start_or_jumpSTART = "start"
 models = ["gpt-3.5-turbo", "gpt-4"]
 triage_versions = ["from_paper", "action_oriented", "outcome_oriented"]
 nr_runs = 1
-special_prompts = {
-    "no_prompt": "",
-    "deontology": ethics_prompts['deontological_prompts']["prompt"] + ''.join(ethics_prompts['deontological_prompts']["steps"]),
-    "utilitarianism": ethics_prompts['utilitarian_prompts']["prompt"] + ''.join(ethics_prompts['utilitarian_prompts']["steps"]),
-}
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description="Run triage experiments with specified conditions")
 parser.add_argument("--model", nargs="*", choices=models, help="Specify model(s) to use")
 parser.add_argument("--triage_version", nargs="*", choices=triage_versions, help="Specify triage version(s) to use")
-parser.add_argument("--special_prompt", nargs="*", choices=special_prompts.keys(), help="Specify special prompt(s) to use")
 parser.add_argument("--nr_runs", type=int, help="Specify how many times you want to run each model per all conditions")
 args = parser.parse_args()
 
@@ -46,8 +41,6 @@ if args.model:
     models = args.model
 if args.triage_version:
     triage_versions = args.triage_version
-if args.special_prompt:
-    special_prompts = {key: special_prompts[key] for key in args.special_prompt}
 if args.nr_runs:
     nr_runs = args.nr_runs
 
@@ -58,46 +51,52 @@ example_output = formatting_prompts['json']['example_json_string']
 
 for model in models: 
   for n in range(nr_runs):
-    for triage_version in triage_versions: 
-      for special_prompt_name, special_prompt in special_prompts.items(): 
+    for triage_version in triage_versions:
+      for persona_name in personas_names: 
         triage_role = triage_zones_and_prompts[triage_version]['role']  # "choose_role"
         triage_zones = ''.join(triage_zones_and_prompts[triage_version]['zones']) # "choose_triage"
         triage_instruction = triage_zones_and_prompts[triage_version]['prompt'] # "choose_prompt"
 
 
-        df[f'{model}_{triage_version}_{special_prompt_name}_reasoning_{n}'] = None
-        df[f'{model}_{triage_version}_{special_prompt_name}_answer_{n}'] = None
+        df[f'{model}_{persona_name}_outcome_oriented_reasoning_{n}'] = None
+        df[f'{model}_{persona_name}_outcome_oriented_answer_{n}'] = None
 
-        context = context + triage_role + triage_zones
+        context = personas['neg'][persona_name] + context + triage_role + triage_zones 
         print(context)
 
         for index, row in df.iterrows():
-          question_prompt = triage_instruction + row['question'] + special_prompt + json_prompt
-          print(question_prompt)
-          print()
-
-          try:
-            completion = client.chat.completions.create(
-                  model= model,
-                  # response_format= { "type": "json_object" },
-                  messages=[
-                      {"role": "system", "content": context},
-                      {"role": "user", "content": question_prompt }
-                    ],
-                  temperature=0
-                  )
-
-            json_response = json.loads(completion.choices[0].message.content)
-            print("answered!", json_response)
+            question_prompt = triage_instruction + row['question'] + json_prompt
+            print(question_prompt)
             print()
 
+            try:
+              completion = client.chat.completions.create(
+                    model= model,
+                    # response_format= { "type": "json_object" },
+                    messages=[
+                        {"role": "system", "content": context},
+                        {"role": "user", "content": question_prompt }
+                      ],
+                    temperature=0
+                    )
 
-            df.loc[index, f'{model}_{triage_version}_{special_prompt_name}_reasoning_{n}'] = json_response["reasoning"]
-            df.loc[index, f'{model}_{triage_version}_{special_prompt_name}_answer_{n}'] = json_response["action"]
-            df.to_csv(f'triage_experiments/datasets/results/triage_results.csv')
+              json_response = json.loads(completion.choices[0].message.content)
+              print("answered!", json_response)
+              print()
 
-          except:
-            print("an exception occured")
+
+              df.loc[index, f'{model}_{persona_name}_{triage_version}_reasoning_{n}'] = json_response["reasoning"]
+              df.loc[index, f'{model}_{persona_name}_{triage_version}_answer_{n}'] = json_response["action"]
+              df.to_csv(f'triage_experiments/datasets/results/triage_results.csv')
+              # break
+            except:
+              # break
+              print("an exception occured")
+              continue
+  #       break
+  #     break
+  #   break
+  # break
 
 print("Done!")
 print(df.head(5))
